d69b27cf74ee24294c4aa02bcc50daf1f1d3f0c3
Elazar Arye <arye9890@gmail.com>
2025-05-21 16:16:00 +0300

    Add constructor to Idf class

--- militaryOperation/Idf.cs
+++ militaryOperation/Idf.cs
@@ -4,6 +4,13 @@
     {
         public DateTime EstablishmentDate { get; set; }
         public string CurrentChiefOfStaff { get; set; }
-        public List<AttackOptions> listOfAttackOptions;
+        public List<AttackOptions> ListOfAttackOptions;
+
+        public Idf(DateTime establishmentDate, string currentChiefOfStaff, List<AttackOptions> listOfAttackOptions)
+        {
+            EstablishmentDate = establishmentDate;
+            CurrentChiefOfStaff = currentChiefOfStaff;
+            ListOfAttackOptions = listOfAttackOptions;
+        }
     }
 }
