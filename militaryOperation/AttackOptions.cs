40b62331d805b3f87d609135b6e0e250d35528c3
yitzchakdamen <yitzchakdamen@gmail.com>
2025-05-25 18:03:46 +0300

    uopdt

---
+++ militaryOperation/AttackOptions.cs
@@ -0,0 +1,29 @@
+﻿namespace MilitaryOperation.Models
+{
+    public abstract  class AttackOptions
+    {
+        public string Name;
+        int AmmunitionCapacity, FuelSupply;
+        Dictionary<string, string> TargetTypeAndWeapon = new();
+        public AttackOptions(string name, int ammunitionCapacity, int fuelSupply)
+        {
+            Name = name;
+            AmmunitionCapacity = ammunitionCapacity;
+            FuelSupply = fuelSupply;
+        }
+        public abstract void Aaaa();
+    }
+
+    public class F16FighterJet : AttackOptions
+    {
+
+        public F16FighterJet(string name, int ammunitionCapacity, int fuelSupply) : base(name, ammunitionCapacity, fuelSupply){  }
+
+        public override void Aaaa()
+        {
+
+        }
+        
+    }
+
+}
